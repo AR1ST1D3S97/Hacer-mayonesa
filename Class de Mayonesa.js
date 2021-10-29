91fd867b70557a863f138c1344dd44634806209c
Aristides <92230561+AR1ST1D3S97@users.noreply.github.com>
2021-10-29 14:55:43 -0300

    Add files via upload

---
+++ Class de Mayonesa.js
@@ -0,0 +1,23 @@
+console.log("MAYONESA CASERA DE ARI")
+class Mayonesa_casera {
+      constructor (huevo, aceite, limon, sal){
+                  this.huevo=huevo;
+                  this.aceite=aceite;
+                  this.limon=limon;
+                  this.sal=sal;
+                  console.log("Para hacer mayonesa casera necesitaremos de " + huevo, aceite, limon,sal);
+      } 
+
+}
+{
+      ingredientes ()
+      return this.mezcla_de_ingredientes();
+
+      mezcla_de_ingredientes ()
+      return this.huevo*this.aceite*this.limon*this*sal;
+
+
+  var mayonesa_casera = new Mayonesa_casera(" 1 huevo, 2 tazas de aceite, 2 gotas de limon, sal al gusto.");{
+  }
+  console.log(mayonesa_casera.ingredientes);
+}
