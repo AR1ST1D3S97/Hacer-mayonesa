a8d8e315fa89a688a439001d54d8480f87d51e09
Aristides <92230561+AR1ST1D3S97@users.noreply.github.com>
2021-11-05 19:32:08 -0300

    Update Class de Mayonesa.js

--- Class de Mayonesa.js
+++ Class de Mayonesa.js
@@ -1,5 +1,5 @@
 console.log("MAYONESA CASERA DE ARI")
-class Mayonesa_casera {
+class Mayonesa_casera {}}
       constructor (huevo, aceite, limon, sal){
                   this.huevo=huevo;
                   this.aceite=aceite;
@@ -17,7 +17,3 @@ class Mayonesa_casera {
       return this.huevo*this.aceite*this.limon*this*sal;
 
 
-  var mayonesa_casera = new Mayonesa_casera(" 1 huevo, 2 tazas de aceite, 2 gotas de limon, sal al gusto.");{
-  }
-  console.log(mayonesa_casera.ingredientes);
-}
